3ac48f8fffa09de801be5f63fd80a21f597ef23f
T-Cog <tcoghlan3@live.ca>
2024-03-07 16:29:01 -0500

    Added Namespace to VFXHandler

--- Assets/Team17/Scripts/VFXHandler.cs
+++ Assets/Team17/Scripts/VFXHandler.cs
@@ -2,60 +2,63 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-public class VFXHandler : MonoBehaviour
+namespace Team17
 {
-    [Header("Heat Levels: (Not adjustable at runtime)")]
-    [SerializeField][Range(0f, 1f)] float heatLevel;
-    [SerializeField][Range(0f, 1f)] float heatLevelCurrent;
-
-    [Header("Object Refs: ")]
-    [SerializeField] Thermostat thermostat;
-    [SerializeField] ParticleSystem snow;
-    [SerializeField] ParticleSystem heat;
-    [SerializeField] VolumeProfile volume;
-
-    [Header("Color Correction:")]
-    [SerializeField] Color heatColor;
-    [SerializeField] Color coldColor;
-
-    [Header("Tuners: ")]
-    [SerializeField][Range(0, 100)] float maxHeatEmission;
-    [SerializeField][Range(0, 100)] float maxColdEmission;
-
-    // Start is called before the first frame update
-    void Start()
+    public class VFXHandler : MonoBehaviour
     {
-        heatLevelCurrent = heatLevel;
-    }
+        [Header("Heat Levels: (Not adjustable at runtime)")]
+        [SerializeField][Range(0f, 1f)] float heatLevel;
+        [SerializeField][Range(0f, 1f)] float heatLevelCurrent;
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Set heat level
-        heatLevel = thermostat.getHeat();
+        [Header("Object Refs: ")]
+        [SerializeField] Thermostat thermostat;
+        [SerializeField] ParticleSystem snow;
+        [SerializeField] ParticleSystem heat;
+        [SerializeField] VolumeProfile volume;
 
-        //Color Correction
-        ColorAdjustments colorAdjustments;
-        volume.TryGet<ColorAdjustments>(out colorAdjustments);
-        float colorR = Mathf.Lerp(coldColor.r, heatColor.r, heatLevel);
-        float colorG = Mathf.Lerp(coldColor.g, heatColor.g, heatLevel);
-        float colorB = Mathf.Lerp(coldColor.b, heatColor.b, heatLevel);
+        [Header("Color Correction:")]
+        [SerializeField] Color heatColor;
+        [SerializeField] Color coldColor;
 
-        colorAdjustments.colorFilter.value = new Color(colorR, colorG, colorB);
+        [Header("Tuners: ")]
+        [SerializeField][Range(0, 100)] float maxHeatEmission;
+        [SerializeField][Range(0, 100)] float maxColdEmission;
 
-        //Snow Effects
-        ParticleSystem.EmissionModule snowEmission = snow.emission;
-        if (heatLevel < 0.5f)
-            snowEmission.rateOverTime = Mathf.SmoothStep(maxColdEmission, 0, heatLevel);
-        else
-            snowEmission.rateOverTime = 0;
-        //Heat effects
-        ParticleSystem.EmissionModule heatEmission = heat.emission;
-        if (heatLevel > 0.5f)
-            heatEmission.rateOverTime = Mathf.SmoothStep(0, maxHeatEmission, heatLevel);
-        else
-            heatEmission.rateOverTime = 0;
+        // Start is called before the first frame update
+        void Start()
+        {
+            heatLevelCurrent = heatLevel;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Set heat level
+            heatLevel = thermostat.getHeat();
+
+            //Color Correction
+            ColorAdjustments colorAdjustments;
+            volume.TryGet<ColorAdjustments>(out colorAdjustments);
+            float colorR = Mathf.Lerp(coldColor.r, heatColor.r, heatLevel);
+            float colorG = Mathf.Lerp(coldColor.g, heatColor.g, heatLevel);
+            float colorB = Mathf.Lerp(coldColor.b, heatColor.b, heatLevel);
+
+            colorAdjustments.colorFilter.value = new Color(colorR, colorG, colorB);
+
+            //Snow Effects
+            ParticleSystem.EmissionModule snowEmission = snow.emission;
+            if (heatLevel < 0.5f)
+                snowEmission.rateOverTime = Mathf.SmoothStep(maxColdEmission, 0, heatLevel);
+            else
+                snowEmission.rateOverTime = 0;
+            //Heat effects
+            ParticleSystem.EmissionModule heatEmission = heat.emission;
+            if (heatLevel > 0.5f)
+                heatEmission.rateOverTime = Mathf.SmoothStep(0, maxHeatEmission, heatLevel);
+            else
+                heatEmission.rateOverTime = 0;
 
 
+        }
     }
 }
